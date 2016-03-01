9fc03e2a645419d103f751a3267f37376cb20125
suvankar <suvankar.17@gmail.com>
2016-03-02 00:45:52 +0530

    added cross for category

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::CategoriesController < Api::V1::BaseController
+  after_filter :cors_set_access_control_headers
+
+def cors_set_access_control_headers
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+  
+end
 
   api :GET, "/api/v1/categories", "Get all Categories"
   example " [
